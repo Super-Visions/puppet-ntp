d2d46dc64fe7c19864724e657cb7201964e6df77
Stefan Goethals <stefan@zipkid.eu>
2012-08-06 16:06:33 +0200

    No defaults, Only act if we have servers configured

--- manifests/config.pp
+++ manifests/config.pp
@@ -47,16 +47,19 @@ class ntp::config(
   $ntp_user       = hiera('ntp_user'),
   $ntp_group      = hiera('ntp_group'),
   $ntp_config_dir = hiera('ntp_config_dir'),
-  $ntp_servers    = hiera('ntp_servers', ['0.pool.ntp.org', '1.pool.ntp.org']),
+  $ntp_servers    = hiera('ntp_servers', []),
   $ntp_options    = hiera('ntp_options', 'UNDEFINED'),
   $ntp_extra_opts = hiera('ntp_extra_opts', 'UNDEFINED')
 )
 {
-  file { "${ntp_config_dir}/ntp.conf":
-    ensure  => present,
-    content => template("${module_name}/ntp.conf.erb"),
-    owner   => $ntp_user,
-    group   => $ntp_group,
-    mode    => '0644',
-  }
+	if( ! empty( $ntp_servers ) )
+	{
+		file { "${ntp_config_dir}/ntp.conf":
+			ensure  => present,
+			content => template("${module_name}/ntp.conf.erb"),
+			owner   => $ntp_user,
+			group   => $ntp_group,
+			mode    => '0644',
+		}
+	}
 }
